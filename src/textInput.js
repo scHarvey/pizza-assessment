a5ec730d04e8b3cfb9019c1acfa5b54488436635
Clint Harvey <sch@rw-dev.com>
2017-09-12 13:48:55 -0500

    More React appropriate way to get the text field value

--- src/textInput.js
+++ src/textInput.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
 export class TextInput extends Component {
     constructor() {
@@ -11,13 +12,13 @@ export class TextInput extends Component {
      * @param {object} _this - reference to the component itself so that we can access things like props. Would generally use an arrow function, but they don't seem to be working as class methods with this config
      */
     handleTextInput (e, _this) {
-        const textValue = document.querySelector('#TextInput');
+        const textValue = ReactDOM.findDOMNode(_this.refs.textInput);
         _this.props.callback(textValue.value);
     }
     render() {
         return (
             <form style={{display: 'inline'}}>
-                <input id="TextInput" type="text" placeholder={this.props.placeholder} onKeyUp={(e) => this.handleTextInput(e, this)} />
+                <input ref="textInput" id="TextInput" type="text" placeholder={this.props.placeholder} onKeyUp={(e) => this.handleTextInput(e, this)} />
             </form>
         );
     }
